7ad2ef1c0f32186d28a067cf462402a5a56cf7d7
Sori01 <72202211+Sori01@users.noreply.github.com>
2021-02-16 20:21:41 +0100

    CostOptimizer Component

---
+++ components/cost_optimizer/src/main/java/CostOptimizer.java
@@ -0,0 +1,84 @@
+public class CostOptimizer {
+
+    private static CostOptimizer instance = new CostOptimizer();
+
+    // Port
+    public Port port;
+
+    private String manufacturer = "<student_name_01> / <student_name_02>";
+    private String type = "team <id>";
+    private String id = "<student_id_01> / <student_id_02>";
+
+    private boolean isOn;
+    //private
+    private int costIndex;
+
+    // Private constructor
+    private CostOptimizer() {
+        port = new Port();
+    }
+
+    // Static method getInstance
+    public static CostOptimizer getInstance() {
+        return instance;
+    }
+
+    // Methods
+    private int innerNeutral() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    private boolean innerOn(){
+
+    }
+
+    private int innerAdd(CheckPoint checkPoint){
+
+    }
+
+    private int innerRemove(int checkPoint){
+
+    }
+
+    private boolean innerValidate(int costIndex){
+
+    }
+
+    private boolean innerOff(){
+
+    }
+
+    // Inner class port
+    public class Port implements ICostOptimizer {
+
+        @Override
+        public String version() {
+            return "CostOptimizer // " + manufacturer + " - " + type + " - " + id;
+        }
+
+        @Override
+        public boolean on() {
+            return innerOn();
+        }
+
+        @Override
+        public int add(CheckPoint checkPoint) {
+            return innerAdd(checkPoint);
+        }
+
+        @Override
+        public int remove(int checkPoint) {
+            return innerRemove(checkPoint);
+        }
+
+        @Override
+        public boolean validate(int costIndex) {
+            return innerValidate(costIndex);
+        }
+
+        @Override
+        public boolean off() {
+            return innerOff();
+        }
+    }
+}
