6789f5d5b3313b5ddb0c36483c7a26577f28cfe5
Sori01 <72202211+Sori01@users.noreply.github.com>
2021-02-16 21:46:05 +0100

    CostOptimizer event

---
+++ src/main/java/event/cost_optimizer/CostOptimizerOn.java
@@ -0,0 +1,10 @@
+package event.cost_optimizer;
+
+public class CostOptimizerOn {
+
+    @Override
+    public String toString() {
+        return "Event: CostOptimizer - On";
+    }
+
+}
---
+++ src/main/java/event/cost_optimizer/CostOptimizerOptimize.java
@@ -0,0 +1,10 @@
+package event.cost_optimizer;
+
+public class CostOptimizerOptimize {
+
+    @Override
+    public String toString() {
+        return "Event: CostOptimizer - Optimize";
+    }
+
+}
