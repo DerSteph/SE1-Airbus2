c033b19488dc8917321d93b93708946fecfdbba0
Sori01 <72202211+Sori01@users.noreply.github.com>
2021-02-16 21:40:59 +0100

    CargoCompartmentLight event

---
+++ src/main/java/event/cargo_compartment_light/CargoCompartmentLightOff.java
@@ -0,0 +1,10 @@
+package event.cargo_compartment_light;
+
+public class CargoCompartmentLightOff {
+
+    @Override
+    public String toString() {
+        return "Event: CargoCompartmentLight - Off";
+    }
+
+}
