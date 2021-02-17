2506655cb15434d106c43b032f83c42c76c8ef52
Sori01 <72202211+Sori01@users.noreply.github.com>
2021-02-17 16:39:00 +0100

    Umbenennung

--- src/main/java/factory/LogoLightFactory.java
+++ src/main/java/factory/LogoLightFactory.java
@@ -2,7 +2,7 @@ package factory;
 
 import configuration.Configuration;
 
-public class LogoLight {
+public class LogoLightFactory {
 
     public static Object build() {
         return Factory.build(Configuration.instance.pathToLogoLightJavaArchive, "LogoLight");
