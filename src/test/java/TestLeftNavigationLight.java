4cf6ce14c3271e8053d9f15a69fc7752494b6d89
Sori01 <72202211+Sori01@users.noreply.github.com>
2021-02-17 17:29:43 +0100

    TestLeftNavigationLight

---
+++ src/test/java/TestLeftNavigationLight.java
@@ -0,0 +1,74 @@
+import factory.LeftNavigationLightFactory;
+import logging.LogEngine;
+import org.junit.jupiter.api.*;
+import recorder.FlightRecorder;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestLeftNavigationLight {
+    private Object componentPort;
+
+    @BeforeEach
+    public void init() {
+        LogEngine.instance.init();
+        FlightRecorder.instance.startup();
+        FlightRecorder.instance.init();
+    }
+
+    @Test
+    @Order(1)
+    public void factory() {
+        componentPort = LeftNavigationLightFactory.build();
+        assertNotNull(componentPort);
+    }
+
+    @Test
+    @Order(2)
+    public void methods() {
+        componentPort = LeftNavigationLightFactory.build();
+        try {
+            Method onMethod = componentPort.getClass().getDeclaredMethod("on");
+            assertNotNull(onMethod);
+
+            Method offMethod = componentPort.getClass().getDeclaredMethod("off");
+            assertNotNull(offMethod);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(3)
+    public void on() {
+        componentPort = LeftNavigationLightFactory.build();
+        try {
+            Method onMethod = componentPort.getClass().getDeclaredMethod("on");
+            boolean isOn = (boolean) onMethod.invoke(componentPort);
+            assertTrue(isOn);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(4)
+    public void off() {
+        componentPort = LeftNavigationLightFactory.build();
+        try {
+            Method offMethod = componentPort.getClass().getDeclaredMethod("off");
+            boolean isOn = (boolean) offMethod.invoke(componentPort);
+            assertFalse(isOn);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    public void close() {
+        FlightRecorder.instance.shutdown();
+        LogEngine.instance.close();
+    }
+}
