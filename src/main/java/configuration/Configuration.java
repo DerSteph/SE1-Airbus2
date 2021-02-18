74b8f7e9ab37e3eefac30c1e9114445f89a52a4f
Nico Hannemann <nico.hannemann@hani-web.de>
2021-02-18 16:49:29 +0100

    Add configuration for apu, engine, gear, hydraulic pump, air conditioning, kitchen

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -22,4 +22,30 @@ public enum Configuration {
     public int numberOfSlat = 6;
 
     // Add new configurations below...
+
+    // apu
+    public String pathToAPUJavaArchive = commonPathToJavaArchive + "apu" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "apu.jar";
+    public int numberOfAPU = 1;
+
+    // engine
+    public String pathToEngineJavaArchive = commonPathToJavaArchive + "engine" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "engine.jar";
+    public int numberOfEngine = 1;
+
+    // gear
+    public String pathToGearJavaArchive = commonPathToJavaArchive + "gear" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "gear.jar";
+    public int numberOfGear = 3;
+
+    // hydraulic_pump
+    public String pathToHydraulicPumpJavaArchive = commonPathToJavaArchive + "hydraulic_pump" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "hydraulic_pump.jar";
+    public int numberOfHydraulicPumpBody = 6;
+    public int numberOfHydraulicPumpWing = 4;
+
+    // air_conditioning
+    public String pathToAirConditioningJavaArchive = commonPathToJavaArchive + "air_conditioning" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "air_conditioning.jar";
+    public int numberOfAirConditioning = 4;
+
+    // kitchen
+    public String pathToKitchenJavaArchive = commonPathToJavaArchive + "kitchen" + fileSeparator + "build" + fileSeparator + "libs" + fileSeparator + "kitchen.jar";
+    public int numberOfKitchen = 3;
+
 }
